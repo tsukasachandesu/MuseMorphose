3e5976dfd410f6763406c8cc72acae4e511f6b47
tsukasachandesu <44788261+tsukasachandesu@users.noreply.github.com>
2023-05-10 15:16:00 +0900

    Update musemorphose.py

--- model/musemorphose.py
+++ model/musemorphose.py
@@ -16,7 +16,6 @@ class VAETransformerDecoder(nn.Module):
     self.d_seg_emb = d_seg_emb
     self.dropout = dropout
     self.activation = activation
-    self.cond_mode = cond_mode
 
     self.seg_emb_proj = nn.Linear(d_seg_emb, d_model, bias=False)
     self.decoder_layers = nn.ModuleList()
@@ -43,7 +42,6 @@ class MuseMorphose(nn.Module):
     dec_dropout=0.1, dec_activation='relu',
     d_rfreq_emb=32, d_polyph_emb=32,
     n_rfreq_cls=8, n_polyph_cls=8,
-    is_training=True
   ):
     super(MuseMorphose, self).__init__()
     self.enc_n_layer = enc_n_layer
@@ -62,7 +60,6 @@ class MuseMorphose(nn.Module):
 
     self.d_vae_latent = d_vae_latent
     self.n_token = n_token
-    self.is_training = is_training
 
     self.token_emb = TokenEmbedding(n_token, d_embed, enc_d_model)
     self.d_embed = d_embed
@@ -104,12 +101,7 @@ class MuseMorphose(nn.Module):
     token_emb = self.token_emb(inp)
     dec_inp = self.emb_dropout(token_emb) + self.pe(inp.size(0))
 
-    if rfreq_cls is not None and polyph_cls is not None:
-      dec_rfreq_emb = self.rfreq_attr_emb(rfreq_cls)
-      dec_polyph_emb = self.polyph_attr_emb(polyph_cls)
-      dec_seg_emb_cat = torch.cat([dec_seg_emb, dec_rfreq_emb, dec_polyph_emb], dim=-1)
-    else:
-      dec_seg_emb_cat = dec_seg_emb
+    dec_seg_emb_cat = dec_seg_emb
 
     out = self.decoder(dec_inp, dec_seg_emb_cat)
     out = self.dec_out_proj(out)
@@ -121,23 +113,18 @@ class MuseMorphose(nn.Module):
 
 
   def forward(self, enc_inp, dec_inp, dec_inp_bar_pos, rfreq_cls=None, polyph_cls=None, padding_mask=None):
-    # [shape of enc_inp] (seqlen_per_bar, bsize, n_bars_per_sample)
     enc_bt_size, enc_n_bars = enc_inp.size(1), enc_inp.size(2)
     enc_token_emb = self.token_emb(enc_inp)
-
-    # [shape of dec_inp] (seqlen_per_sample, bsize)
-    # [shape of rfreq_cls & polyph_cls] same as above 
-    # -- (should copy each bar's label to all corresponding indices)
+    
     dec_token_emb = self.token_emb(dec_inp)
 
     enc_token_emb = enc_token_emb.reshape(
       enc_inp.size(0), -1, enc_token_emb.size(-1)
     )
+    
     enc_inp = self.emb_dropout(enc_token_emb) + self.pe(enc_inp.size(0))
     dec_inp = self.emb_dropout(dec_token_emb) + self.pe(dec_inp.size(0))
 
-    # [shape of padding_mask] (bsize, n_bars_per_sample, seqlen_per_bar)
-    # -- should be `True` for padded indices (i.e., those >= seqlen of the bar), `False` otherwise
     if padding_mask is not None:
       padding_mask = padding_mask.reshape(-1, padding_mask.size(-1))
 
@@ -147,17 +134,10 @@ class MuseMorphose(nn.Module):
 
     dec_seg_emb = torch.zeros(dec_inp.size(0), dec_inp.size(1), self.d_vae_latent).to(vae_latent.device)
     for n in range(dec_inp.size(1)):
-      # [shape of dec_inp_bar_pos] (bsize, n_bars_per_sample + 1)
-      # -- stores [[start idx of bar #1, sample #1, ..., start idx of bar #K, sample #1, seqlen of sample #1], [same for another sample], ...]
       for b, (st, ed) in enumerate(zip(dec_inp_bar_pos[n, :-1], dec_inp_bar_pos[n, 1:])):
         dec_seg_emb[st:ed, n, :] = vae_latent_reshaped[n, b, :]
 
-    if rfreq_cls is not None and polyph_cls is not None and self.use_attr_cls:
-      dec_rfreq_emb = self.rfreq_attr_emb(rfreq_cls)
-      dec_polyph_emb = self.polyph_attr_emb(polyph_cls)
-      dec_seg_emb_cat = torch.cat([dec_seg_emb, dec_rfreq_emb, dec_polyph_emb], dim=-1)
-    else:
-      dec_seg_emb_cat = dec_seg_emb
+    dec_seg_emb_cat = dec_seg_emb
 
     dec_out = self.decoder(dec_inp, dec_seg_emb_cat)
     dec_logits = self.dec_out_proj(dec_out)
